9366cfe1af271396ce6502b90e6963d19c572c42
rukhshida <rukhshidailyas@gmail.com>
2024-06-30 07:55:00 +0545

    learn data types

---
+++ index.js
@@ -0,0 +1,209 @@
+// variables
+// var x = 90;
+// console.log(x);
+// var x = 100;
+// console.log (x);
+
+// let
+// let y = 600;
+// console.log(y);
+// y = 340;
+// console.log(y);
+
+// const (decalre at once)
+// const result = 90;
+// console.log(result);
+
+// const result 50;
+// crossOriginIsolated.log(result);
+
+// data types in javascript
+// const output = "simar simar";
+// console.log(output);
+// console.log(typeof output);
+
+// Number 
+// const result = 77;
+// console.log(result);
+// console.log(typeof result);
+
+// Float
+// const x = 4.5;
+// console
+
+// undefined
+// let y;
+// console.log(y);
+// console.log(typeof y);
+
+// null
+// const x = null;
+// console.log(x);
+// console.log(typeof x);
+
+// Boolean
+// const isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue);
+
+// BigInt
+// const result = 786gh;
+// console.log(result);
+// console.log(typeof result);
+
+// symbol
+// const output = symbol("hello world");
+// console.log(output);
+//  console.log(typeof output);
+
+// let x =6.54;
+// console.log(x);
+// consolw.log(typeof x);
+
+// // type conversion
+// const result = "2334"
+// console.log(result);
+// const output = Number(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result = "2334"
+// const output = parseInt(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result = "2334"
+// const output = parseFloat(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result = "2334"
+// const output = String(result);
+// console.log(output);
+// console.log(typeof output)
+
+// const result = "2334"
+// const output = result.toString();
+// console.log(output);
+// console.log(typeof output);
+
+// const isMarried = false;
+// console.log(isMarried);
+// console.log(ans);
+// console.log(typeof isMarried);
+
+// Object types
+// const obj = {
+//     name:"Shamma Noor",
+//     Age: 21,
+//     isMarried: false,
+//     occupation: "Ceo",
+// };
+// console.log(obj);
+// console.log(typeof obj);
+
+// const arr = [3,4,5,6];
+// console.log(arr);
+// console.log(typeof arr);
+
+// const date = new Date()
+// console.log(date)
+// console.log(typeof date);
+
+// const result = 1 + "1"
+// console.log(result);
+// console.log(typeof result);
+
+
+// const result = 4 - "2"
+// console.log(result);
+// console.log(typeof result);
+
+// string method
+// const result = "hello home";
+// // length
+// console.log(result.length);
+
+// // charAt
+// console.log(result.charAt(2));
+
+// // indexof
+// console.log(result.indexOf("o"));
+
+// // last indexof
+// console.log(result.lastIndexOf("o"));
+
+// // lowrcase
+// console.log(result.toLowerCase());
+
+// // uppercase
+// console.log(result.toUpperCase());
+
+// // replace
+// console.log(result.replace("home","everyone"));
+
+// // includes
+// console.log(result.includes("homes"));
+
+// // slice
+// console.log(result.slice(1,4));
+
+// // concat
+// console.log(result.concat("amazing javascript"));
+
+// let output = "     hello  students";
+// console.log(output.trim());
+
+// // split
+// console.log(result.split(""));
+
+// concatination
+
+// let  x = "Hello world";
+// let y = "i Love js!";
+// console.log(x + " " + y);
+
+// const a = 30;
+// const b = 50;
+// const result = a + b;
+// console.log("The sum of number is:" + result);
+// console.log('The sum of two number is: ${result}');
+
+// let y = 70;
+// y = y+10;
+// console.log(y);
+
+// let x = 70;
+// x += 10;
+// console.log(x);
+
+
+// comparison operator
+// let a = 10;
+// let b = 5;
+// let c = "10";
+// console.lolg(a == b);
+// console.log(a === b);
+// console.log(a !=b);
+// console.log(a !=c);
+// console.log(a>b);
+// console.log(a < b);
+
+
+// ternary operator
+// let ageGroup = 18;
+// console.log(ageGroup>=18 ? "you can vote" : "you can't");
+
+// // unary operator
+// let x = "123";
+// let y = +x  //positive
+// console.log(y);
+// console.log(typeof y);
+
+// const obj = {
+//     name:"sama";
+//     age: 12,
+// };
+// delete obj.age;
+// console.log(obj);
+
